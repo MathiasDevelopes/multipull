6ce0e8414955ce673c3fbc45aba2f7ad4bbbd8c1
Matti <mat8nil49@gmail.com>
2021-12-20 21:01:19 +0100

    Spelling fix

--- multipull.py
+++ multipull.py
@@ -7,7 +7,7 @@ path = os.getcwd()
 # Puts all folders within the current directory with sub-folder .git into a list.
 githubFolders = [os.path.join(path, item) for item in os.listdir(path) if os.path.isdir(os.path.join(path, item, ".git"))]
 
-# Iterates through every github folder from the githubFolders list, and executes git pull comamnd on them.
+# Iterates through every folder from the githubFolders list, and executes the git pull comamnd on them.
 for folder in githubFolders:
     print(f"Executing git pull on {folder}")
     os.chdir(folder)
