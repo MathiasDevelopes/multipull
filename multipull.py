f25d53c93c9e3b2f1d5bab892b64f28b17b513cc
Matti <mat8nil49@gmail.com>
2021-12-23 20:24:13 +0100

    Added custom directory support
    Added custom directory support, and added some other small things.

--- multipull.py
+++ multipull.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 import os
 
-# Gets the current working directory
+#Sets the current working directory, but if custom_dir_path is specified and valid, it will be overwritten with custom path.
 path = os.getcwd()
 
-# Puts all folders within the current working directory with sub-folder .git into a list.
+# Checks if the user wants to choose a custom directory.
+custom_dir_choice = input("Would you like to specify a custom dir (y/n): ").lower()
+if custom_dir_choice == "y":
+    custom_dir_path = input("Enter the custom directory path: ")
+    if os.path.isdir(custom_dir_path):
+        path = custom_dir_path
+    else:
+        print("Directory not valid, falling back to current working directory.")
+
+
+# Puts all folders within the path with sub-folder .git into a list.
 githubFolders = [os.path.join(path, item) for item in os.listdir(path) if os.path.isdir(os.path.join(path, item, ".git"))]
+
+#Checks if there are no github folders in the path directory.
+if len(githubFolders) == 0:
+    print("There are no github folders in this directory, quitting.")
+    exit(0)
 
 # Iterates through every folder from the githubFolders list, and executes the git pull comamnd on them.
 for folder in githubFolders:
